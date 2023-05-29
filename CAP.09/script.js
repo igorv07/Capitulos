3b58f6939594277c558039d524d1d26b55e6f216
igorv07 <126192452+igorv07@users.noreply.github.com>
2023-05-29 15:18:49 -0300

    Add files via upload

---
+++ CAP.09/script.js
@@ -0,0 +1,14 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Digite a primeira nota: ', (nota1) => {
+  rl.question('Digite a segunda nota: ', (nota2) => {
+    const media = (parseFloat(nota1) + parseFloat(nota2)) / 2;
+    console.log('A média das notas é: ' + media.toFixed(1));
+    rl.close();
+  });
+});
